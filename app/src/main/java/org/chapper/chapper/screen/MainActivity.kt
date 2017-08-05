1bd7e4724227bdfe9f39dc1514e6ab5384f66cbf
ZZooRM <zzoorm@gmail.com>
2017-08-05 16:42:57 +0300

    Change icon

--- app/src/main/java/org/chapper/chapper/screen/MainActivity.kt
+++ app/src/main/java/org/chapper/chapper/screen/MainActivity.kt
@@ -5,23 +5,24 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import io.underdark.Underdark
 import io.underdark.transport.TransportKind
+import io.underdark.util.Identity
 import org.chapper.chapper.R
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    var mTextView: TextView = findViewById(R.id.textviw) as TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Underdark.configureTransport(554554345,
-                1,
-                null, null,
+        val mTextView: TextView = findViewById(R.id.textviw) as TextView
+
+        val kek = Underdark.configureTransport(554554345,
+                Identity.generateNodeId(), null, null,
                 this,
                 EnumSet.of(TransportKind.WIFI,
                         TransportKind.BLUETOOTH))
-                .start()
+
+        kek.start()
 
         mTextView.setOnClickListener {
 
